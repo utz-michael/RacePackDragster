8cb970055504cbc3d49617c0e010d26be8b2ea79
utz-michael <michael.utz@ndsatcom.com>
2015-01-22 22:30:15 +0100

    Spannung kalibriert inkl verpolung und netzfilter

--- RacePackDragster.ino
+++ RacePackDragster.ino
@@ -399,7 +399,7 @@ FuelMainPSI = (FuelMain - FuelMainCal)* 0.140056;
 FuelCarburtorPSI = (FuelCarburtor - FuelCarburtorCal)* 0.140056;
 FuelNOSPSI = (FuelNOS - FuelNOSCal)* 0.140056;
 MAPPSI = (MAP - MAPCal)* 0.0919963;
-BordspannungVolt = (BordSpannung  * 0.0196)+ 0.339;
+BordspannungVolt = (BordSpannung  * 0.0196)+ 0.839;
 
 
 char buffer[40];
